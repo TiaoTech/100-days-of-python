65dac1d3e44486155ad87439d6735b659a1b989f
Tiao <tiao.devx@gmail.com>
2025-04-12 00:53:43 -0400

    Add Day 2 Project - Tip Calculator Project
    This projects will calculate the total amount each person has to pay after a tip has been added.

---
+++ day-2-tip-calculator.py
@@ -0,0 +1,14 @@
+# Calculates the amount each person would pay, after the tip has been added. 
+
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+
+tip_percent_calculation = tip / 100
+total_tip = bill * tip_percent_calculation
+total_bill = bill + total_tip
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
+
+print(f"Each person will pay ${final_amount}")
